ddc89ee5a88a230a64e2faaf419b5a3ee5a581b0
naufal <naufalhaxor78@gmail.com>
2020-03-13 10:57:09 +0700

    Menambahkan fungsi menu dan melengkapi fungsi operator bilangan

--- Calculator.cs
+++ Calculator.cs
@@ -2,23 +2,102 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
-			int a = 10;
-			int b = 6;
-			
-			Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
-			Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
-			
-			Console.WriteLine("\nTekan sembarang key untuk keluar");
-			Console.ReadKey();
-	}	
-	
-	static int Penambahan(int a, int b)
-	{
-		return a + b;
-	}
-	
-	static int Pengurangan(int a, int b)
-	{
-		return a - b;
-	}
+        {
+            Console.Title= "Aplikasi Kalkulator" ;
+            Menu();
+            Console.WriteLine();
+            Console.Write("Input Nomor Menu [1-4]: ");
+            int pil = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            switch (pil)
+            {
+                case 1:
+                    Jumlah();
+                    Console.WriteLine();
+                    Console.WriteLine("Tekan sembarang key untuk keluar");
+                    break;
+                case 2:
+                    Kurang();
+                    Console.WriteLine();
+                    Console.WriteLine("Tekan sembarang key untuk keluar");
+                    break;
+                case 3:
+                    Kali();
+                    Console.WriteLine();
+                    Console.WriteLine("Tekan sembarang key untuk keluar");
+                    break;
+                case 4:
+                    Bagi();
+                    Console.WriteLine();
+                    Console.WriteLine("Tekan sembarang key untuk keluar");
+                    break;
+                default:
+                    Console.WriteLine("Maaf menu tidak tersedia");
+                    Console.WriteLine();
+                    Console.WriteLine("Tekan sembarang key untuk keluar");
+                    break;
+            }
+            Console.ReadKey();
+        }
+        public static void Menu()
+        {
+            Console.WriteLine("Pilih Menu Calculator : ");
+            Console.WriteLine();
+            Console.WriteLine("1. Penjumlahan");
+            Console.WriteLine("2. Pengurangan");
+            Console.WriteLine("3. Perkalian");
+            Console.WriteLine("4. Pembagian");
+        }
+        public static void Jumlah()
+        {
+            Console.Write("Masukkan nilai a = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Masukkan nilai b = ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Hasil Penambahan dari " + a + " + " + b + " = " + Penjumlahan(a, b));
+        }
+        public static void Kurang()
+        {
+            Console.Write("Masukkan nilai a = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Masukkan nilai b = ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Hasil Pengurangan dari " + a + " - " + b + " = " + Pengurangan(a, b));
+        }
+        public static void Kali()
+        {
+            Console.Write("Masukkan nilai a = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Masukkan nilai b = ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Hasil Perkalian dari " + a + " x " + b + " = " + Perkalian(a, b));
+        }
+        public static void Bagi()
+        {
+            Console.Write("Masukkan nilai a = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Masukkan nilai b = ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Hasil Pembagian dari " + a + " / " + b + " = " + Pembagian(a, b));
+        }
+        static int Penjumlahan(int a, int b)
+        {
+            return a + b;
+        }
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+        static int Pembagian(int a, int b)
+        {
+            return a / b;
+        }
 }
